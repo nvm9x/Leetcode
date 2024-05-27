a7f13db5ba06cd006351ed0c5bc14733b6a73036
Zeinep <Zeinep>
2024-05-27 22:44:52 +0600

    Remove final

--- src/ProblemSolving/RemoveDuplicates.java
+++ src/ProblemSolving/RemoveDuplicates.java
@@ -3,27 +3,26 @@ package ProblemSolving;
 import java.util.Arrays;
 
 public class RemoveDuplicates {
-    public int removeDuplicated(int [] nums){
-        int k=0;
-        if(nums.length==0){
+    public int removeDuplicated(int [] nums) {
+        int j = 1;
+        if (nums.length == 0) {
             return 0;
         }
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length-1;i++) {
-            for (int j = 1 + i; j < nums.length; j++) {
-                if (nums[i] != nums[j]) {
-                    i = j - 1;
-                    break;
-
-                }
-
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i-1] != nums[j]) {
+                nums[j]=nums[i];
+                j++;
             }
-            k++;
+
+
+        }
+        return j;
+
         }
 
-        return k;
 
-    }
+
+
 
     public static void main(String[] args) {
         int [] array = {0,0,1,1,1,2,2,3,3,4};
